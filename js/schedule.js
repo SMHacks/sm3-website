a48773eecc46e7e13ddff3c6bf382bf7b119ac8b
Emmanuel <ecobian12310@gmail.com>
2019-09-08 19:55:39 -0700

    Made schedule resemble los alto's site
    The design could use a lot of work, but the features are there. The arrows can navigate through the schedule items and there's an active class on each item to let users know which event they're looking at.

--- js/schedule.js
+++ js/schedule.js
@@ -1,11 +1,60 @@
+var satTimes = document.getElementsByClassName('sat-time');
+var satEvents = document.getElementsByClassName('sat-event');
+var sunTimes = document.getElementsByClassName('sun-time');
+var sunEvents = document.getElementsByClassName('sun-event');
+var tableRows = document.getElementsByClassName('tr');
+var i = 0;
+
+function plusDivs(n) {
+	tableRows[i].className = tableRows[i].className.replace(" active", "");
+	n == -1 ? tableRows[i - 1].className += " active" : tableRows[i + 1].className += " active";
+	i += n;
+	showContent();
+}
+
+function showContent() {
+	if (i < 0) {
+		i = 0;
+	}
+	else if (i >= 0 && i < satTimes.length) {
+		document.getElementById('event').innerHTML = satEvents[i].innerHTML;
+		document.getElementById('time').innerHTML = 'Saturday, ' + satTimes[i].innerHTML;
+		viewSat();
+	}
+	else if (i >= satTimes.length && i < satTimes.length + sunTimes.length) {
+		document.getElementById('event').innerHTML = sunEvents[i - satEvents.length].innerHTML;
+		document.getElementById('time').innerHTML = 'Sunday, ' + sunTimes[i - satTimes.length].innerHTML;
+		viewSun();
+	}
+	else if (i >= satTimes.length + sunTimes.length) {
+		i = satTimes.length + sunTimes.length - 1;
+	}
+}
+
+function viewSun() {
+	$('#scheduleSun').show();
+	$('#scheduleSat').hide();
+}
+
+function viewSat() {
+	$('#scheduleSun').hide();
+	$('#scheduleSat').show();	
+}
+
 $(document).ready(function(){
 	$('#scheduleSun').hide();
 	$('#viewSun').click(function() {
-		$('#scheduleSun').show();
-		$('#scheduleSat').hide();
+		viewSun();
+		tableRows[i].className = tableRows[i].className.replace(" active", "");
+		i = satTimes.length;
+		tableRows[i].className += " active";
+		showContent();
 	})
 	$('#viewSat').click(function() {
-		$('#scheduleSun').hide();
-		$('#scheduleSat').show();
+		viewSat();
+		tableRows[i].className = tableRows[i].className.replace(" active", "");
+		i = 0;
+		tableRows[i].className += " active";
+		showContent();
 	})
 })
